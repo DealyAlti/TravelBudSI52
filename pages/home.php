fe7ef82bd6f9755315caabdb9340e391762446db
Dealy Alti <dealyalti_2226240037@mhs.mdp.ac.id>
2024-09-24 20:25:59 +0700

    Membuat header dan footer

---
+++ pages/home.php
@@ -0,0 +1,29 @@
+<?php
+include '../include/header.php'; 
+?>
+
+<?php
+include '../include/footer.php'; 
+?>
+
+<style>
+    header {
+        background-color: #000080;
+        color: #FFFFE0;
+        padding: 10px;
+        text-align: center;
+    }
+
+
+    footer {
+        text-align: center;
+        padding: 10px;
+        background-color: #000080;
+        color: #FFFFE0;
+        width: 100%;
+        position: relative;
+        margin-top: 20px;
+    }
+
+
+</style>
